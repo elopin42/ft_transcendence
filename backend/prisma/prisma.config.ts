bb4a0b6da5f2bc4490a4f191f91eff3823225d71
ethan lopin <ethanlo0506@gmail.com>
2026-04-01 00:11:00 +0200

    ethan: mise a jours des npm

--- backend/prisma/prisma.config.ts
+++ backend/prisma/prisma.config.ts
@@ -1,12 +1,5 @@
-// import { PrismaClient } from '@prisma/client';
-//
-// export const prisma = new PrismaClient ({
-//   adapter: {
-//     url: process.env.DATABASE_URL,
-//   },
-// });
-
-
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient ({
+});
+
