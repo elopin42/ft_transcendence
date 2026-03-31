f77121a597263219b71f903665baa9c7ddd24310
ethan lopin <ethanlo0506@gmail.com>
2026-03-31 21:05:25 +0200

    ethan: BUG! compile pas / ajout de prisma qui communique avec la db donc model de user cree

---
+++ backend/prisma/prisma.config.ts
@@ -0,0 +1,12 @@
+// import { PrismaClient } from '@prisma/client';
+//
+// export const prisma = new PrismaClient ({
+//   adapter: {
+//     url: process.env.DATABASE_URL,
+//   },
+// });
+
+
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient();
