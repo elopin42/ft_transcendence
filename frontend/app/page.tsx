c266ff29c6ccab1e7dc8a71b6447199278e34472
babapuissance <nassimbariol@gmail.com>
2026-04-06 00:13:26 +0200

    feat: landing page with MSP-style button and title

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,25 @@
-import { redirect } from 'next/navigation'
+import './landing.css';
 
 export default function Home() {
-    redirect("/login");
-  }
-
+  return (
+    <div className="landing-wrapper">
+      <img className="logo-topleft" src="/logomsp.png" alt="" />
+      <div className="title-container">
+        <h1 className="subtitle-backer">Friends, Fun, code &amp; Connect</h1>
+        <h1 className="subtitle-front">Friends, Fun, code &amp; Connect</h1>
+      </div>
+      <div className="button">
+        <a className="main-btn-msp pink-button" href="/login">
+          Play MovieStarParis 42
+        </a>
+        <div className="button-art">
+          <img className="bling" src="/blingstar.png" style={{ top: '10%', left: '10%' }} alt="" />
+          <img className="bling" src="/blingstar.png" style={{ top: '-15%', left: '15%' }} alt="" />
+          <img className="bling" src="/blingstar.png" style={{ top: '-30%', left: '28%' }} alt="" />
+          <img className="bling" src="/blingstar.png" style={{ bottom: '5%', right: '31%' }} alt="" />
+          <img className="logo" src="/logomsp.png" style={{ top: '-35%', right: '0%' }} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
